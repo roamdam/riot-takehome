f6845d693bdab855de1e9f3d55cc6966037a61e5
Romain <damian.romain@gmail.com>
2026-01-27 14:00:54 +0100

    :books: Add API docstrings

--- api/services/signature.py
+++ api/services/signature.py
@@ -12,12 +12,42 @@ blueprint_signature = Blueprint("signature", import_name="__name__")
 @blueprint_signature.route("/sign", methods=["POST"])
 def sign():
     """
-    Sign
+    Generate signature for received JSON payload.
 
     ---
     post:
-        summary: Encrypt a JSON object
-        description: Encrypts all depth-1 values of the provided JSON object.
+        summary: Generate signature for a JSON object
+        requestBody:
+            required: true
+            content:
+                application/json:
+                    schema:
+                        type: object
+                        additionalProperties: true
+                    example:
+                        name: Alice
+                        age: 32
+        responses:
+            200:
+                description: Successfully generated signature
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                signature:
+                                    type: string
+                        example:
+                            signature: 'a1b2c3d4e5f6g7h8i9j0'
+            400:
+                description: Invalid input payload (payload is not a JSON object)
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                error:
+                                    type: string
         tags:
             - signature
     """
@@ -31,12 +61,46 @@ def sign():
 @blueprint_signature.route("/verify", methods=["POST"])
 def verify():
     """
-    Verify
+    Verify data within payload against provided signature.
 
     ---
     post:
-        summary: Encrypt a JSON object
-        description: Encrypts all depth-1 values of the provided JSON object.
+        summary: Verify signature for a JSON object
+        description: >
+            Verify that the `data` object provided matches with the signature provided.
+            Verification is not dependant on the order of keys within the data object.
+        requestBody:
+            required: true
+            content:
+                application/json:
+                    schema:
+                        type: object
+                        properties:
+                            signature:
+                                type: string
+                            data:
+                                type: object
+                                additionalProperties: true
+                        required:
+                            - signature
+                            - data
+                    example:
+                        signature: 'a1b2c3d4e5f6g7h8i9j0'
+                        data:
+                            name: Alice
+                            age: 32
+        responses:
+            204:
+                description: Verification successful
+            400:
+                description: Invalid signature or invalid data
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                error:
+                                    type: string
         tags:
             - signature
     """
@@ -50,6 +114,6 @@ def verify():
     # Generate signature from data and compare to provided signature
     signature = handler.sign(payload[SignatureFields.data])
     if signature != payload[SignatureFields.signature]:
-        return {"error": "Invalid signature"}, HTTPStatus.BAD_REQUEST
+        return {"error": "Invalid signature or data"}, HTTPStatus.BAD_REQUEST
     else:
         return "", HTTPStatus.NO_CONTENT
