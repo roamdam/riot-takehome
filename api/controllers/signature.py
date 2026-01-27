e144af2b90d0058d56a906a2c959adab9fbcae21
Romain <damian.romain@gmail.com>
2026-01-27 10:12:23 +0100

    :pencil: Add placeholders files

---
+++ api/controllers/signature.py
@@ -0,0 +1,8 @@
+
+class SignatureHandler:
+
+    def sign(self, payload: dict) -> dict:
+        return payload
+
+    def verify(self, payload: dict) -> dict:
+        return payload
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from api.config.apispec import spec
 from api.config.definitions import API_URL, SWAGGER_URL, ROOT_URL
 from api.services.encryption import blueprint_encryption
+from api.services.signature import blueprint_signature
 from api.services.swagger import swagger_ui_blueprint
 
 
@@ -12,6 +13,8 @@ logger.debug("Start API Service")
 
 app = Flask(__name__)
 app.register_blueprint(blueprint_encryption, url_prefix=ROOT_URL)
+app.register_blueprint(blueprint_signature, url_prefix=ROOT_URL)
+
 
 with app.test_request_context():
     for fn_name, fn_view in app.view_functions.items():
@@ -29,7 +32,6 @@ def swagger():
 
 
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
-
 
 if __name__ == "__main__":
     app.run()
